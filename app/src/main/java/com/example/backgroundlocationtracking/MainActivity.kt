16b46bd4f9ddf68bc9156ee045c0a73be14bf350
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-07-05 01:27:04 +0300

    add service

--- app/src/main/java/com/example/backgroundlocationtracking/MainActivity.kt
+++ app/src/main/java/com/example/backgroundlocationtracking/MainActivity.kt
@@ -1,18 +1,63 @@
- package com.example.backgroundlocationtracking
+package com.example.backgroundlocationtracking
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.example.backgroundlocationtracking.ui.theme.BackgroundLocationTrackingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            0
+        )
+
         enableEdgeToEdge()
         setContent {
             BackgroundLocationTrackingTheme {
-
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, LocationService::class.java).apply {
+                            action = LocationService.ACTION_START
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "Start")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        Intent(applicationContext, LocationService::class.java).apply {
+                            action = LocationService.ACTION_STOP
+                            startService(this)
+                        }
+                    }) {
+                        Text(text = "Stop")
+                    }
+                }
             }
         }
     }
