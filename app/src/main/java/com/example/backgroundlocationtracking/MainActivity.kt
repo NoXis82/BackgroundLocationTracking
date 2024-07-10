59b81a747e76c6c0e9853a6e05105d4f0f940454
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-07-10 09:43:52 +0300

    add request permission post notification

--- app/src/main/java/com/example/backgroundlocationtracking/MainActivity.kt
+++ app/src/main/java/com/example/backgroundlocationtracking/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.backgroundlocationtracking
 
 import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -20,6 +22,7 @@ import androidx.core.app.ActivityCompat
 import com.example.backgroundlocationtracking.ui.theme.BackgroundLocationTrackingTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,6 +31,7 @@ class MainActivity : ComponentActivity() {
             arrayOf(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.POST_NOTIFICATIONS
             ),
             0
         )
